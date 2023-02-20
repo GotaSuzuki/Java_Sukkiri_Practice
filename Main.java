233933d0a29978b614b8d5feafa15c82a846b896
GotaSuzuki <go.baseball.0408@icloud.com>
2023-02-20 15:23:37 +0900

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.time.*;
+import java.time.format.*;
+public class Main {
+  public static void main(String[] args){
+    
+  }
+}
